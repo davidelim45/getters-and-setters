1473556701d28182f6cc7c076f3e5fa022f8459c
david.elim45 <davidelim170204@gmail.com>
2023-12-01 10:05:16 +0100

    getters and setters

---
+++ src/novproekt/MYCLASS.java
@@ -0,0 +1,12 @@
+package novproekt;
+
+public class MYCLASS {
+
+	public static void main(String[] args) {
+    car v1= new car();
+		v1.setColor("Red");
+		System.out.println(v1.getColor());
+
+	}
+
+}
